52bf9a386357ecdbda5cd26e607e93aad0192c40
Anders Strømberg <andestro@stud.ntnu.no>
2019-02-19 14:05:51 +0100

    ignore curved paths

--- svg2GDS.py
+++ svg2GDS.py
@@ -24,9 +24,8 @@ def main(fileName, sizeOfTheCell, outName):
     # Fetch namespaces (xmlns:)
     svg_namespaces = dict([node for _, node in ET.iterparse(fileName, events=['start-ns'])])
 
-    # Fetch all layers
+    # Fetch all layers (assuming top g's are layers)
     layers = root.findall('svg:g', svg_namespaces)
-
 
     pathcells = []
     for layerNum, layer in enumerate(layers):
@@ -46,8 +45,20 @@ def main(fileName, sizeOfTheCell, outName):
     layout.save(outName)
 
 def pathToCell(cell, path):
+    curved_segments = ['c','s','q','t','a']
+    curved_segments += [c.upper() for c in curved_segments]
+
     print('Adding path to cell')
     print(path.attrib['id'])
+    directions = path.attrib['d'].split()
+    
+    if any(x in directions for x in curved_segments):
+        print('ERROR: Curved lines in path not supported. Skipping.')
+        return
+    for d in directions:
+        print(d)
+        # TODO: implement creation of boundary
+
 
 if __name__ == "__main__":
     args = sys.argv
